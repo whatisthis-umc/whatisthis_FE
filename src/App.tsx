e558f0a3d2a30b5b17a5962489f77a0457e85c05
bini0918 <165636716+bini0918@users.noreply.github.com>
2025-07-07 19:42:36 +0900

    footer 컴포넌트

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import MainPage from "./pages/MainPage";
+import Navbar from "./components/Navbar";
+import TipsPage from "./pages/TipsPage";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div>
-      <BrowserRouter>
+    <BrowserRouter>
+      <div className="min-h-screen flex-col">
+        <Navbar />
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path="/tips" element={<TipsPage />} />
         </Routes>
-      </BrowserRouter>
-    </div>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
