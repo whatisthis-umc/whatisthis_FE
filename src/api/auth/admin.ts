b28c5246b09f10616cf40c89d18d99224ce56b87
bini0918 <165636716+bini0918@users.noreply.github.com>
2025-07-27 21:19:32 +0900

    feat/admin-login-api
    관리자 로그인 api 연동

---
+++ src/api/auth/admin.ts
@@ -0,0 +1,9 @@
+// 관리자 로그인 api
+import { axiosInstance} from "../axiosInstance";
+
+export const adminLogin = async (username:string, password:string) : Promise<string> =>{
+    const res = await axiosInstance.post('/admin/login', {username, password});
+
+if (!res.data.isSuccess) throw new Error(res.data.message);
+return res.data.result.accessToken;
+};
