d280e04d46fbadc9a0dbd5716400f88130745d38
jooyoung <kjy030609@inha.edu>
2025-08-05 15:39:35 +0900

    api 호출 변수 삭제

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 const baseURL = import.meta.env.VITE_API_BASE_URL;
+
+
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: baseURL,
   headers: {
     "Content-Type": "application/json",
   },
