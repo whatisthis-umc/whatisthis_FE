33375126a2383630ed02b91c5171b24b19c58272
oohrii <jodie415@naver.com>
2025-08-15 04:35:28 +0900

    고객센터 공지사항 필독블록 추가

--- src/pages/customer/NoticeListPage.tsx
+++ src/pages/customer/NoticeListPage.tsx
@@ -92,6 +92,32 @@ const NoticeListPage = () => {
             <div className="flex items-start justify-between w-full">
               <div className="flex-1">
                 <div className="flex flex-col gap-3 md:gap-6">
+                  {/* 필독 블록 */}
+                  <div
+                    style={{
+                      display: "flex",
+                      padding: "4px 12px",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      borderRadius: "32px",
+                      border: "1px solid var(--WIT-Gray200, #999)",
+                      alignSelf: "flex-start",
+                    }}
+                  >
+                    <span
+                      style={{
+                        color: "var(--WIT-Gray600, #333)",
+                        fontFamily: "Pretendard",
+                        fontSize: "20px",
+                        fontStyle: "normal",
+                        fontWeight: 500,
+                        lineHeight: "150%",
+                        letterSpacing: "-0.4px",
+                      }}
+                    >
+                      필독
+                    </span>
+                  </div>
                   <h3
                     className="transition-colors text-left"
                     style={{
